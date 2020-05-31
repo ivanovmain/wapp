8e25dab5b23ea702ca9e8c68e01fa171c4ecd448
Ivanov Vladislav <ivanovvvmain@yandex.ru>
2020-05-31 17:06:55 +0700

    Updates

---
+++ precache-manifest.9cd008222b7e4e81b0dabac5e762e548.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af68b4c02ea65e4d6f501a082eb792d",
+    "url": "/wapp/index.html"
+  },
+  {
+    "revision": "354d82e601a469d6d908",
+    "url": "/wapp/static/js/2.4ec365b6.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/wapp/static/js/2.4ec365b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cfae162704848ea05e",
+    "url": "/wapp/static/js/main.b34b9aa6.chunk.js"
+  },
+  {
+    "revision": "db7ace71711899cc2906",
+    "url": "/wapp/static/js/runtime-main.9ab324d0.js"
+  }
+]);
